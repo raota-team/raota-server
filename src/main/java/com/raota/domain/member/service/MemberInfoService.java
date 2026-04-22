e380e84009e9c97ce5b3b55f08590b03027ea8de
HuitaePark <qkrgmlxo3174@gmail.com>
2026-04-22 18:06:37 +0900

    fix: MemberInfoService 내 FileUploader 임포트 누락 수정

--- src/main/java/com/raota/domain/member/service/MemberInfoService.java
+++ src/main/java/com/raota/domain/member/service/MemberInfoService.java
@@ -7,6 +7,7 @@ import com.raota.domain.member.controller.response.VisitSummaryResponse;
 import com.raota.domain.member.model.MemberProfile;
 import com.raota.domain.member.repository.MemberRepository;
 import com.raota.domain.ramenShop.repository.RamenProofPictureRepository;
+import com.raota.global.file.FileUploader;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
