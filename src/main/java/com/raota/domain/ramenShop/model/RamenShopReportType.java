79a72a20e5820fa1ca3f0e9534fed6d04d35fcf2
HuitaePark <qkrgmlxo3174@gmail.com>
2026-04-24 11:00:51 +0900

    feat: 라멘샵 제보 이벤트 메뉴 제보 메뉴 추가

--- src/main/java/com/raota/domain/ramenShop/model/RamenShopReportType.java
+++ src/main/java/com/raota/domain/ramenShop/model/RamenShopReportType.java
@@ -9,7 +9,8 @@ public enum RamenShopReportType {
     OPENING_HOURS_ERROR("영업시간 오류"),
     CLOSED("폐업"),
     MENU_INFO_ERROR("메뉴정보 오류"),
-    OTHERS("기타");
+    OTHERS("기타"),
+    EVENT("이벤트 메뉴 제보");
 
     private final String description;
 }
